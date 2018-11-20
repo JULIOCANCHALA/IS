e29dde666c499e3a4b83f9732e2a5434ddab12bf
JULIOCANCHALA <juliofernando1c@hotmail.com>
2018-11-20 03:05:39 +0100

    Preview login commit

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 @app.errorhandler(404)#Error pages
 def page_not_found(e):
