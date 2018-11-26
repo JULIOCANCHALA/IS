d9f471ac41bf50d1d96bd2147c70336d255d0499
JULIOCANCHALA <juliofernando1c@hotmail.com>
2018-11-26 02:58:57 +0100

    Preview Signin commit

--- app.py
+++ app.py
@@ -5,16 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='JON')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='LogIn')
 
+@app.route('/signup')
+def signup():
+    return render_template('signuptype.html', title='SignIn')
+
+@app.route('/signuptype')
+def signuptype():
+    return render_template('signuptype.html', title='SignIn')
+
+@app.route('/signupCompany')
+def signupperson():
+    return render_template('signupCompany.html', title='SignIn')
+
+@app.route('/signupPerson')
+def signupcompany():
+    return render_template('signupPerson.html', title='SignIn')
 
 @app.errorhandler(404)#Error pages
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template('404.html', title='404'),404
 
 
 if __name__ == '__main__':
